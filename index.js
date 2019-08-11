32a3383fad08b22764d18c84c0f247d4616cfc27
jarvma18 <markusmjarvinen@outlook.com>
2019-08-11 21:21:19 +0300

    Conversion from single precision to decimal and vice versa works, the code needs refactoring..

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict'
 
+function createBias(exponent) {
+  return Math.pow(2, exponent - 1) - 1;
+}
+
+function createSign(value) {
+  if (value === 0) {
+    value = 1 / value;
+  }
+  return value ? value < 0 ? -1 : 1 : 0;
+}
+
 function createUnsignedBinaryString(value, unsignedLength) {
   let binary = (value >>> 0).toString(2);
   let unsigned = '';
@@ -34,10 +45,11 @@ function createBinaryString(value, bits, isLowEndian) {
   return binary;
 }
 
-function precisionToDecimal(value, bits, mantissa, bias) {
+function precisionToDecimal(value, bits, mantissa) {
   let sign = null;
   let exponent = '';
   let fraction = 0;
+  let bias = createBias(bits - mantissa - 1);
   for (let i = 0; i < bits; i++) {
     if (value[i] !== '0' && value[i] !== '1') {
       throw new Error('Binary string is invalid');
@@ -56,20 +68,10 @@ function precisionToDecimal(value, bits, mantissa, bias) {
   }
   exponent = parseInt(exponent, 2);
   if (exponent === 0 && fraction === 0) {
-    if (sign === 0) {
-      return 0.0;
-    }
-    else if (sign === 1) {
-      return -0.0;
-    }
+    return Math.pow(-1, sign) * 0.0;
   }
   if (exponent === 255 && fraction === 0) {
-    if (sign === 0) {
-      return Infinity;
-    }
-    else if (sign === 1) {
-      return -Infinity;
-    }
+    return Math.pow(-1, sign) * Infinity;
   }
   if (exponent === 255 && fraction !== 0) {
     return NaN;
@@ -78,6 +80,76 @@ function precisionToDecimal(value, bits, mantissa, bias) {
     return Math.pow(-1, sign) * (fraction) * Math.pow(2, -126);
   }
   return Math.pow(-1, sign) * (1 + fraction) * Math.pow(2, (exponent - bias));
+}
+
+function decimalToPrecision(value, bits, mantissa) {
+  let sign = createSign(value) === 1 ? 0 : 1;
+  let bias = createBias(bits - mantissa - 1);
+  value = Math.pow(-1, sign) * value;
+  if (value < 1) {
+    let counter = 0;
+    while (value < 1) {
+      counter++;
+      value = ((value / Math.pow(2, -1 * counter)) >= 1 &&
+              (value / Math.pow(2, -1 * counter)) < 2) ?
+              (value / Math.pow(2, -1 * counter)) : value;
+    }
+    let exponent = parseInt(-1 * counter + bias);
+    exponent = createUnsignedBinaryString(exponent, 8);
+    let startFraction = value - parseInt(value);
+    let currentValue = startFraction;
+    let fraction = '';
+    for (let i = 0; i < mantissa; i++) {
+      currentValue = currentValue * 2;
+      fraction += (parseInt(currentValue) >>> 0).toString();
+      if (currentValue > 1) {
+        currentValue = currentValue - 1;
+      }
+      if (currentValue === startFraction || currentValue === 1) {
+        if (fraction[i] === '0' && currentValue > 0.05) {
+          fraction = fraction.slice(0, i - 1);
+          fraction += '1';
+        }
+        for (let j = 0; j < (mantissa - i); j++) {
+          fraction += '0';
+        }
+        break;
+      }
+      if (i === mantissa - 1) {
+        if (fraction[i] === '0' && currentValue > 0.05) {
+          fraction = fraction.slice(0, i);
+          fraction += '1';
+        }
+      }
+    }
+    return sign + exponent + fraction;
+  }
+  else {
+    let integer = (parseInt(value) >>> 0).toString(2);
+    let exponent = parseInt(integer.length - 1 + bias);
+    exponent = createUnsignedBinaryString(exponent, 8);
+    let startFraction = value - parseInt(value);
+    let currentValue = startFraction;
+    let fraction = '';
+    for (let i = 0; i < mantissa; i++) {
+      currentValue = currentValue * 2;
+      fraction += (parseInt(currentValue) >>> 0).toString();
+      if (currentValue > 1) {
+        currentValue = currentValue - 1;
+      }
+      if (currentValue === startFraction || currentValue === 1) {
+        if (fraction[i] === '0' && currentValue > 0.05) {
+          fraction[i] = '1';
+        }
+        for (let j = 0; j < (mantissa - i); j++) {
+          fraction += '0';
+        }
+        break;
+      }
+    }
+    fraction = (integer + fraction).slice(1, 24);
+    return sign + exponent + fraction;
+  }
 }
 
 exports.halfToDecimal = function halfToDecimal() {
@@ -101,14 +173,15 @@ exports.singleToDecimal = function singleToDecimal(value, isLowEndian) {
     }
   }
   try {
-    return precisionToDecimal(binaryString, 32, 23, 127)
+    return precisionToDecimal(binaryString, 32, 23)
   }
   catch(error) {
     console.log(error);
   }
 };
-exports.decimalToSingle = function decimalToSingle(value, isLowEndian, returnType) {
 
+exports.decimalToSingle = function decimalToSingle(value, isLowEndian, returnType) {
+  return decimalToPrecision(value, 32, 23);
 };
 
 exports.doubleToDecimal = function doubleToDecimal() {
