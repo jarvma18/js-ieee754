729a62ce53c4f259795cd9255a2a681fdba775b4
Markus Järvinen <markusmjarvinen@outlook.com>
2026-02-08 22:10:41 +0000

    Changed the jest.config.cjs to prefer TypeScript.

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'json', 'ts', 'tsx', 'jsx', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
