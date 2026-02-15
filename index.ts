d97e2052b76d15a659c31df7dd32e63e568e4480
Markus Järvinen <markusmjarvinen@outlook.com>
2026-02-15 21:33:20 +0000

    Moved general utils functions to utils file.

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import * as decimalPrecision from './src/decimalPrecision';
 import * as precisionDecimal from './src/precisionDecimal';
 import * as validationHelpers from './src/validationHelpers';
-import * as shared from './src/utils';
+import * as utils from './src/utils';
 
-type PrecisionMode = 'half' | 'single' | 'double';
+export type PrecisionMode = 'half' | 'single' | 'double';
 type ReturnType = '8bitArray' | '16bitArray';
 
 type GetPrecisionOptions = {
@@ -15,48 +15,12 @@ type GetDecimalOptions = {
   mode: PrecisionMode;
 };
 
-function createBinaryString(value: number[], bits: number): string {
-  let signed = value.length;
-  let binary = '';
-  let unsigned = bits / signed;
-  for (let i = 0; i < signed; i++) {
-    binary += shared.createUnsignedBinaryString(value[i], unsigned);
-  }
-  return binary;
-}
-
-function createInt(value: string, bits: number): number[] {
-  let array: number[] = [];
-  for (let i = 0; i < (value.length / bits); i++) {
-    let binary = '';
-    for (let j = i * bits; j < ((i + 1) * bits); j++) {
-      binary += value[j];
-    }
-    array.push(parseInt(binary, 2));
-  }
-  return array;
-}
-
-function setDefaultBitsForPrecision(precision: PrecisionMode): number {
-  if (precision === 'half') { return 16; }
-  if (precision === 'single') { return 32; }
-  if (precision === 'double') { return 64; }
-  return 16;
-}
-
-function setDefaultMantissaForPrecision(precision: PrecisionMode): number {
-  if (precision === 'half') { return 10; }
-  if (precision === 'single') { return 23; }
-  if (precision === 'double') { return 52; }
-  return 10;
-}
-
 function precisionToType(precision: string, options?: GetPrecisionOptions): string | number[] {
   if (options && options.returnType === '16bitArray') {
-    return createInt(precision, 16);
+    return utils.createInt(precision, 16);
   }
   else if (options && options.returnType === '8bitArray') {
-    return createInt(precision, 8);
+    return utils.createInt(precision, 8);
   }
   else {
     return precision;
@@ -66,9 +30,9 @@ function precisionToType(precision: string, options?: GetPrecisionOptions): stri
 export function getDecimal(value: string | number[], options: GetDecimalOptions): number | undefined {
   try {
     validationHelpers.validate('precisionDecimal', value, options);
-    const bits = setDefaultBitsForPrecision(options.mode);
-    const mantissa = setDefaultMantissaForPrecision(options.mode);
-    let binaryValue = typeof value === 'object' ? createBinaryString(value, bits) : value;
+    const bits = utils.setDefaultBitsForPrecision(options.mode);
+    const mantissa = utils.setDefaultMantissaForPrecision(options.mode);
+    let binaryValue = typeof value === 'object' ? utils.createBinaryString(value, bits) : value;
     return precisionDecimal.precisionToDecimal(binaryValue, bits, mantissa);
   }
   catch(err) {
@@ -79,8 +43,8 @@ export function getDecimal(value: string | number[], options: GetDecimalOptions)
 export function getPrecision(value: number, options: GetPrecisionOptions): string | number[] | undefined {
   try {
     validationHelpers.validate('decimalPrecision', value, options);
-    const bits = setDefaultBitsForPrecision(options.mode);
-    const mantissa = setDefaultMantissaForPrecision(options.mode);
+    const bits = utils.setDefaultBitsForPrecision(options.mode);
+    const mantissa = utils.setDefaultMantissaForPrecision(options.mode);
     let precision = decimalPrecision.decimalToPrecision(value, bits, mantissa);
     return precisionToType(precision, options);
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { PrecisionMode } from '..';
+
 export function createBias(exponent: number): number {
   return Math.pow(2, exponent - 1) - 1;
 }
@@ -9,3 +11,40 @@ export function createUnsignedBinaryString(value: number, unsigned: number): str
   }
   return binary;
 }
+
+export function createBinaryString(value: number[], bits: number): string {
+  let signed = value.length;
+  let binary = '';
+  let unsigned = bits / signed;
+  for (let i = 0; i < signed; i++) {
+    binary += createUnsignedBinaryString(value[i], unsigned);
+  }
+  return binary;
+}
+
+export function createInt(value: string, bits: number): number[] {
+  let array: number[] = [];
+  for (let i = 0; i < (value.length / bits); i++) {
+    let binary = '';
+    for (let j = i * bits; j < ((i + 1) * bits); j++) {
+      binary += value[j];
+    }
+    array.push(parseInt(binary, 2));
+  }
+  return array;
+}
+
+export function setDefaultBitsForPrecision(precision: PrecisionMode): number {
+  if (precision === 'half') { return 16; }
+  if (precision === 'single') { return 32; }
+  if (precision === 'double') { return 64; }
+  return 16;
+}
+
+export function setDefaultMantissaForPrecision(precision: PrecisionMode): number {
+  if (precision === 'half') { return 10; }
+  if (precision === 'single') { return 23; }
+  if (precision === 'double') { return 52; }
+  return 10;
+}
+
