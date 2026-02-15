441104614ba8fec70eca3eb8b1de16afd7cb179c
Markus Järvinen <markusmjarvinen@outlook.com>
2026-02-15 21:09:01 +0000

    Extracted getDecimal tests +  refactoring.

---
+++ getDecimal.test.js
@@ -0,0 +1,27 @@
+const ieee754 = require('./index');
+
+describe('getDecimal', () => {
+  const roundTripCases = [
+    // 1000.5 in various modes/formats
+    ['half', [25553], 1000.5],
+    ['single', [17530, 8192], 1000.5],
+    ['double', [16527, 17408, 0, 0], 1000.5],
+    ['half', [99, 209], 1000.5],
+    ['single', [68, 122, 32, 0], 1000.5],
+    ['double', [64, 143, 68, 0, 0, 0, 0, 0], 1000.5],
+    ['half', '0110001111010001', 1000.5],
+    ['single', '01000100011110100010000000000000', 1000.5],
+    [
+      'double',
+      '0100000010001111010001000000000000000000000000000000000000000000',
+      1000.5,
+    ],
+  ];
+
+  test.each(roundTripCases)(
+    'mode %s input %p returns %p',
+    (mode, input, expected) => {
+      expect(ieee754.getDecimal(input, {mode})).toBe(expected);
+    }
+  );
+});
